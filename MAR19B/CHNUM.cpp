34060cf9475ae9acfd5d2cf46c97724dec73bf80
Sunny Kumar <sun42nykumarsk@gmail.com>
2019-03-02 14:54:41 +0530

    Add files via upload

---
+++ MAR19B/CHNUM.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int t, n, temp, c1, c2;
+    cin >> t;
+
+    while(t--)
+    {
+        c1=0, c2=0;
+        cin >> n;
+        for(auto i=0; i<n; i++)
+        {
+            cin >> temp;
+            (temp>0)?c1++:c2++;
+        }
+        (c1==n || c2==n) ? cout << n << " " << n << endl : (c1>=c2) ? cout << c1 << " " << c2 << endl : cout << c2 << " " << c1 << endl;
+    }
+    
+	return 0;
+}
